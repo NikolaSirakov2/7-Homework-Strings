6b9fc0c75c1598f62adf0bcc11c20a03307b2bfe
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-24 09:50:21 +0200

    Create 01.js

---
+++ 01.js
@@ -0,0 +1,7 @@
+let stringOne = "Evga";
+let stringTwo = "Delta";
+
+console.log(stringOne.toUpperCase());
+console.log(stringOne.toLowerCase());
+console.log(stringTwo.toUpperCase());
+console.log(stringTwo.toLowerCase());
