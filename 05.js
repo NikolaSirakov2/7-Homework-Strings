ede9ae03d8be43b0ac6e9f1a567f84ba5812cf91
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-25 11:57:16 +0200

    Update 05.js

--- 05.js
+++ 05.js
@@ -6,14 +6,17 @@ let vertical = "";
 for(let i = 0; i < word2.length; i++){
     
     if(word1.includes(word2[i])){
-        for(let k = 0; k < word1; k++){
+        for(let k = 0; k < word1.length; k++){
             if(word1[k] === word2[i]){
                 vertical += (word2 + "\n");
             } else {
                 vertical += (word1[k] + "\n");
             }
+
         }
+        
     }
+    break;
 }
 
 console.log(vertical);
