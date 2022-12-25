9072371edae72d30e3fca63765d29b1d4c112ab1
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-25 11:26:31 +0200

    Create 07.js

---
+++ 07.js
@@ -0,0 +1,12 @@
+let string = "agro meter datascaner declaration";
+let words = string.split(" ");
+let count = words.length;
+let longestWord = words[0].length;
+
+for(let i = 1; i < words.length; i++){
+    if(words[i].length > words[i-1].length){
+        longestWord = words[i].length;
+    }
+}
+
+console.log(`${count} думи, най-дългата е с ${longestWord} символа.`);
