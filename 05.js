8a4d142df5af82dd94b6d62db0630b157f9e7eb1
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-25 12:06:24 +0200

    Update 05.js

--- 05.js
+++ 05.js
@@ -1,22 +1,19 @@
 let word1 = "машина";
-let word2 = "шапка";
+let word2 = "мапка";
 
 let vertical = "";
 
-for(let i = 0; i < word2.length; i++){
-    
-    if(word1.includes(word2[i])){
-        for(let k = 0; k < word1.length; k++){
-            if(word1[k] === word2[i]){
-                vertical += (word2 + "\n");
-            } else {
-                vertical += (word1[k] + "\n");
-            }
-
-        }
-        
+for (let i = 0; i < word2.length; i++) {
+  if (word1.includes(word2[i])) {
+    for (let k = 0; k < word1.length; k++) {
+      if (word1[k] === word2[i]) {
+        vertical += word2 + "\n";
+      } else {
+        vertical += word1[k] + "\n";
+      }
     }
     break;
+  }
 }
 
 console.log(vertical);
