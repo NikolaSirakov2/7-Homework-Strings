5465f43a5f3d8ccd1901f949b081f0c1c1cf23ba
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-26 11:44:37 +0200

    Update 14.js

--- 14.js
+++ 14.js
@@ -1,11 +1,15 @@
-function valueOfFactorial(n){
-   
+let n = "4";
+
+function valueOfFactorial(a){
+    if(typeof a !== "number"){
+        return null;
+    } else {
         let factorial = 1;
-        for(let i = n; i > 0; i--){
+        for(let i = a; i > 0; i--){
             factorial *= i;
         }
         return factorial;
-    
+    }
 }
 
-console.log(valueOfFactorial(1));
+console.log(valueOfFactorial(n));
