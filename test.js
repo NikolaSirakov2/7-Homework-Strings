c49f47283a053ccd65acadb0114fb13b713f85c2
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-29 11:03:59 +0200

    Update test.js

--- test.js
+++ test.js
@@ -4,11 +4,11 @@ let b = 6;
 let arr = [5,6]
 
 function swap(arr){
-    arrey = [1,2,3]
+    arr = [1,2,3]
     let temp = arr[0];
     arr[0] = arr[1];
     arr[1] = temp;
-    console.log(arrey);
+    console.log(arr);
 }
 
 swap(arr);
