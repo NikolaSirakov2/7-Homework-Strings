99e2ce8ed72728a1094973c25be29444c21745c3
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-29 11:10:33 +0200

    Update 13.js

--- 13.js
+++ 13.js
@@ -1,17 +1,25 @@
 let arr1 = [3, 5, 9];
-let arr2 = [1, 2, 8];
+let arr2 = [1, 2, "no"];
 
-function stackAllElements(a, b){
-    let newArr = [];
-    for(let j = 0; j < a.length; j++){
-        newArr.push(a[j]);
+function stackAllElements(a, b) {
+  let newArr = [];
+  for (let j = 0; j < a.length; j++) {
+    if (typeof arr1[j] !== "number") {
+      return null;
+    } else {
+      newArr.push(a[j]);
     }
+  }
 
-    for(let m = 0; m < b.length; m++){
-        newArr.push(b[m]);
+  for (let m = 0; m < b.length; m++) {
+    if (typeof arr2[m] !== "number") {
+      return null;
+    } else {
+      newArr.push(b[m]);
     }
+  }
 
-    return newArr;
+  return newArr;
 }
 
-console.log(stackAllElements(arr1,arr2))
+console.log(stackAllElements(arr1, arr2));
